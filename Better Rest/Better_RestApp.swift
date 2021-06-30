c6a96d4b00612e69d5b82bed7fe8090408a75daf
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-06-30 11:12:45 +0530

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Nivas Muthu M G on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
